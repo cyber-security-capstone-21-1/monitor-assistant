75adc5ef1885664e4c3927a56232a2cf20990d81
G1 <g1.jeon@kakao.com>
2021-03-28 21:23:12 +0900

    modified: Elasticsearch Service - AWS applied

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Dashboard extends Component {
     render () {
         return (
             <>
                 <header>대시보드</header>
-                <iframe src="http://localhost:5601"></iframe>
+                <iframe src="https://search-monitor-assistant-es-q7phnn6tbzv2usgv7qxouxloxu.ap-northeast-2.es.amazonaws.com/_plugin/kibana/app/dashboards#/view/0986bcd0-8fc0-11eb-98b3-cb77ad4fc98b?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))"></iframe>
                 <iframe src="http://localhost:8080/api/monitor/?keyword=테스트"></iframe>
             </>
         )
