2cdca7e0b5c664025e31fc634f42cbb0ae4f99be
G1 <g1.jeon@kakao.com>
2021-04-16 19:18:55 +0900

    modified: Aside, functional Component, hooks, removed: classifier, add: Component / Route (IntList)

---
+++ frontend/src/pages/IntList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageHeader from '@/components/PageHeader/PageHeader';
+
+function IntList (props) {
+    return (
+        <>
+            <PageHeader title="첩보 목록" desc="첩보 목록" />
+        </>
+    );
+}
+
+export default IntList;
