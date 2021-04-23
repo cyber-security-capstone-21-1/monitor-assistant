3124d9b921f845bcc33dfb58cc1e66fab2962bfa
junghyun2ee <ju2173@ajou.ac.kr>
2021-04-23 14:32:25 +0900

    test2

---
+++ src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/entities/ScrapeEntity.java
@@ -0,0 +1,11 @@
+package kr.ac.ajou.cybersecurity.capstone5.monitorassistant.entities;
+
+public class ScrapeEntity {
+    private String site;
+    private String title;
+    private String created_at;
+    private String author;
+    private String content;
+    private String url;
+    private String type;
+}
