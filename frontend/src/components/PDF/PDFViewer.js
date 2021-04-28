2a415fd263307dd8396a367ec28bacc6af2211c1
KIM HONGJU <ghdwn0120@ajou.ac.kr>
2021-04-28 12:39:22 +0900

    pdf

--- frontend/src/components/PDF/PDFViewer.js
+++ frontend/src/components/PDF/PDFViewer.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 // import SinglePageViewer from './types/single-page';
-import IntelligenceCreator from './types/Intelligence-creator';
+import IntelligenceCreator from "./types/Intelligence-creator";
 
 import "./PDFViewer.scss";
 
-function PDFViewer (props) {
-    return (
-        <section>
-            <article>
-                {/* <SinglePageViewer pdf={props.file} /> */}
-                <IntelligenceCreator />
-            </article>
-        </section>
-    );
+function PDFViewer(props) {
+  return (
+    <section>
+      <article>
+        {/* <SinglePageViewer pdf={props.file} /> */}
+        <IntelligenceCreator />
+      </article>
+    </section>
+  );
 }
 
 export default PDFViewer;
