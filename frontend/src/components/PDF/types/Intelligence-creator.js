130686d6089bfd8892269f3e8df025aad9be8a91
G1 <g1.jeon@kakao.com>
2021-04-30 14:59:30 +0900

    add: PDF Viewer Boilerplate

--- frontend/src/components/PDF/types/Intelligence-creator.js
+++ frontend/src/components/PDF/types/Intelligence-creator.js
@@ -9,54 +9,77 @@ import {
   StyleSheet,
 } from "@react-pdf/renderer";
 
+import TestImage from "@/assets/images/dog1.jpg";
+import Logo from "@/assets/images/logo.png";
+import { ExtraLight, Light, RText, Medium, SemiBold, Bold, Regular } from "@/assets/fonts/IBMPlexSansKR";
+
 export default function IntelligenceCreator(props) {
-  Font.register({
-    family: "nanumGothic",
-    src: "https://fonts.gstatic.com/ea/nanumgothic/v5/NanumGothic-Regular.ttf",
-  });
 
   Font.register({
-    family: "nanumGothicBold",
-    src: "https://fonts.gstatic.com/ea/nanumgothic/v5/NanumGothic-Bold.ttf",
+    family: "IBMPlexSansKR",
+    fonts: [
+      { src: ExtraLight, fontWeight: 100 },
+      { src: Light, fontWeight: 200 },
+      { src: RText, fontWeight: 300 },
+      { src: Regular, fontWeight: 400 },
+      { src: Medium, fontWeight: 500 },
+      { src: SemiBold, fontWeight: 600 },
+      { src: Bold, fontWeight: 700 },
+    ]
   });
 
   const styles = StyleSheet.create({
-    table: {
-      //   border: 1,
-    },
-    image: {
-      marginVertical: 15,
-      marginHorizontal: 100,
-    },
-    body: {
+    pageDefault: {
+      fontFamily: "IBMPlexSansKR",
+      fontWeight: 200,
+      fontSize: 12,
       paddingTop: 35,
       paddingBottom: 65,
       paddingHorizontal: 35,
     },
-    title: {
-      fontSize: 30,
-      textAlign: "left",
-      color: "#2d2e30",
-      fontFamily: "nanumGothicBold",
-      marginBottom: 40,
-      marginLeft: 20,
-      display: "block",
-    },
-    text: {
-      margin: 12,
-      fontSize: 14,
-      textAlign: "justify",
-      fontFamily: "nanumGothic",
-      marginBottom: 30,
-      display: "block",
+    logo: {
+      width: 30,
+      height: 30,
+      marginRight: 5
     },
     header: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flex-end",
+      alignContent: "center",
       fontSize: 10,
+      fontWeight: 600,
       marginBottom: 20,
-      textAlign: "right",
       color: "#81848a",
-      fontFamily: "nanumGothic",
-      display: "block",
+      fontFamily: "IBMPlexSansKR",
+    },
+    title: {
+      fontSize: 30,
+      fontWeight: 700,
+      textAlign: "left",
+      marginBottom: 40,
+      marginLeft: 20
+    },
+    table: {
+      maxWidth: "100%",
+      overflowWrap: "break-word"
+    },
+    tableRow: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alignItems: "center",
+      marginBottom: 10
+    },
+    tableCellHead: {
+      width: 100,
+      fontWeight: 400,
+      verticalAlign: "middle"
+    },
+    tableCellBody: {
+      width: "calc(100% - 100)",
+      fontSize: 10,
+      verticalAlign: "middle"
     },
     pageNumber: {
       position: "absolute",
@@ -69,143 +92,105 @@ export default function IntelligenceCreator(props) {
     },
   });
   return (
+    // PDF Document Starts
     <Document>
-      <Page style={styles.body}>
-        <Text style={styles.header} fixed>
-          사이버수사관 모니터링 보조 업무 시스템
-        </Text>
-        <Text style={styles.title} fixed>
-          게시물 모니터링 위험 징후 보고
-        </Text>
-
-        <table style={styles.table}>
-          <tr>
-            <th width="150">
-              <Text style={styles.text}>첩보명</Text>
-            </th>
-            <td width="500">
-              <Text style={styles.text}>
-                {"학폭 사건의 피해 호소 주장 글로 의심되는 게시물 게재 관련 "}
-              </Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>첩보 유형</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>{"피해 호소 주장"}</Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>관련 법</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>{"정보통신망법"}</Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>게시물명</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>
-                {"과거 학폭으로 인한 트라우마, 나쁜 선택을 하려는 나"}
-              </Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>사이트</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>{"네이트 판"}</Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>작성자</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>{"oo(닉네임)"}</Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>작성일</Text>
-            </th>
-            <td>
-              {" "}
-              <Text style={styles.text}>
-                {"2021.04.04 09:08 (UTC +09:00, Seoul)"}
-              </Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>URL</Text>
-            </th>
-            <td>
-              {" "}
-              <Text style={styles.text}>
-                {"https://pann.nate.com/talk/358871562"}
-              </Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>첩보 내용</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>
-                {
-                  "최근 연쇄적으로 발생되고 있는 학교폭력(줄임말 '학폭') 폭로 사건과 관련하여 새롭게 추가된 피해 호소 주장글로, 과거에 받았던 피해와 그 고통에 대해서 호소 하는 내용으로 이루어져 있음."
-                }
-              </Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>수사관 의견</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>
-                {
-                  "'학폭'사건이 연쇄적으로 발생, 최근 사회적 공분을 일으키고 있는 사건으로 관심도가 상당하기에, 관련하여 지속적으로 관련 키워드 모니터링을 집중 수행하며, 가짜뉴스 유포 등의 의심 징후가 발견될 경우 즉시 내사 진행"
-                }
-              </Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>내사 진행 여부</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>{"보류"}</Text>
-            </td>
-          </tr>
-          <tr>
-            <th>
-              <Text style={styles.text}>페이지 스냅샷</Text>
-            </th>
-            <td>
-              <Text style={styles.text}>
-                {
-                  "https://monitor-asissistant-ap-northeast-2a.s3.amazonnaws.com/..."
-                }
-              </Text>
-            </td>
-          </tr>
-
-          <Text
-            style={styles.pageNumber}
-            render={({ pageNumber, totalPages }) =>
-              `${pageNumber} / ${totalPages}`
-            }
-            fixed
-          />
-        </table>
+      {/* A4 Page */}
+      <Page wrap={false} style={styles.pageDefault}>
+        {/* Header */}
+        <View style={styles.header}>
+          <Image style={styles.logo} src={Logo}/>
+          <View>
+            <Text>사이버수사관모니터링</Text>
+            <Text>업무보조시스템</Text>
+          </View>
+        </View>
+        {/* Title */}
+        <View>
+          <Text style={styles.title}>
+            게시물 모니터링 위험 징후 보고
+          </Text>
+        </View>
+        {/* Content */}
+        {/* Table */}
+        <View style={styles.table}>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>첩보명</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>학폭 사건의 피해 호소 주장 글로 의심되는 게시물 게재 관련</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>수사관</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>전지원 (대원)</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>첩보 유형</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>피해 호소 주장</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>관련 법률</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>정보통신망법 > ...</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>게시물명</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>과거 학폭으로 인한 트라우마, 나쁜 선택을 하려는 나</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>사이트</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>네이트 판</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>작성자</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>ㅇㅇ (닉네임)</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>작성일</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>2021. 04. 09 09:08 (UTC +09:00, Seoul)</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>URL</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>https://pann.nate.com/talk/358871562</Text>
+          </View>
+          {/* Row */}
+          <View style={styles.tableRow}>
+            {/* Header */}
+            <Text style={styles.tableCellHead}>첩보 내용</Text>
+            {/* Body */}
+            <Text style={styles.tableCellBody}>최근 연쇄적으로 발생되고 있는 학교 폭력 (줄임말 '학폭') 폭로 사건과 관련하여 새롭게 추가된 피해 호소 주장글로, 과거에 받았던 피해와 그 고통에 대해서 호소하는 내용으로 이루어져 있음.</Text>
+          </View>
+          {/* Screenshot */}
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCellHead}>스크린샷</Text>
+            <Image style={styles.tableCellBody} src="https://cscapstone-21-1-5-s3.s3.ap-northeast-2.amazonaws.com/SS1.jpg" />
+          </View>
+          {/* End of Table */}
+        </View>
+        {/* End of Content */}
       </Page>
     </Document>
   );
