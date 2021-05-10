1c9ec965af19c10a0830e3dfde5587edd14ff195
lsh <hehesi2007@ajou.ac.kr>
2021-05-10 17:08:55 +0900

    회원가입 중복 수정 객체형식 맞춤

--- frontend/src/pages/auth/SignUp.js
+++ frontend/src/pages/auth/SignUp.js
@@ -38,35 +38,36 @@ export default function SignUp() {
       return;
     }
 
+    delete data.passwordConfirm;
     //회원가입 성공과 동시에 로그인
-    axios
-      .post(`${Constants.ENDPOINT}/api/auth/emailvalidity`, data)
+    axios.post(`${Constants.ENDPOINT}/api/auth/emailvalidity`, data)
       .then((res) => {
-        if (res) {
+        if (!res) {
           // 회원가입 가능
           axios
             .post(`${Constants.ENDPOINT}/api/auth/signup`, data)
             .then((res) => {
-                console.log("회원가입 가능 signup 응답값 : ", res);
-                axios.post(`${Constants.ENDPOINT}/api/auth/login`, data).then((response) => {
-                    const { accessToken } = response.data;
-                    axios.defaults.headers.common[
+              console.log("회원가입 가능 signup 응답값 : ", res);
+              axios
+                .post(`${Constants.ENDPOINT}/api/auth/login`, data)
+                .then((response) => {
+                  const { accessToken } = response.data;
+                  axios.defaults.headers.common[
                     "Authorization"
-                    ] = `Bearer ${accessToken}`;
-                    AuthenticationService.registerSuccessfulLoginForJwt(
+                  ] = `Bearer ${accessToken}`;
+                  AuthenticationService.registerSuccessfulLoginForJwt(
                     data.email,
                     accessToken
-                    );
-                    setAuthInfo("", "");
-                }).catch((error) => {
-                    Swal.fire({
+                  );
+                  setAuthInfo("", "");
+                })
+                .catch((error) => {
+                  Swal.fire({
                     title: "로그인 실패",
                     icon: "error",
-                    });
-                    console.error(error);
+                  });
+                  console.error(error);
                 });
-
-
             })
             .catch(console.log);
         } else {
@@ -86,54 +87,54 @@ export default function SignUp() {
   const loggedIn = AuthenticationService.isUserLoggedIn();
   return (
     <>
-    {!loggedIn ? (
+      {!loggedIn ? (
         <>
-      <header className="title">회원가입</header>
-      <div className="form__item_wrapper">
-        <div className="form__item">
-          <legend>이메일</legend>
-          <input
-            type="email"
-            onChange={handleAuthInfo}
-            name="email"
-            required
-            placeholder="이메일을 입력하세요"
-          />
-          <span className="error">{emailMsg}</span>
-        </div>
-        <div className="form__item">
-          <legend>패스워드</legend>
-          <input
-            type="password"
-            onChange={handleAuthInfo}
-            name="password"
-            required
-            placeholder="패스워드를 입력하세요"
-          />
-          <span className="error">{passMsg}</span>
-        </div>
-        <div className="form__item">
-          <legend>패스워드 확인</legend>
-          <input
-            type="password"
-            onChange={handleAuthInfo}
-            name="passwordConfirm"
-            required
-            placeholder="다시 한번 입력하세요"
-          />
-          <span className="error">{passwdMsg}</span>
-        </div>
-        <div className="form__item">
-          <input type="submit" value="회원가입" onClick={onSignUp} />
-        </div>
-        <hr />
-        <p className="info">
-          <Link to="/auth/login" className="btn">
-            로그인 화면으로 돌아가기
-          </Link>
-        </p>
-      </div>
-      </>
+          <header className="title">회원가입</header>
+          <div className="form__item_wrapper">
+            <div className="form__item">
+              <legend>이메일</legend>
+              <input
+                type="email"
+                onChange={handleAuthInfo}
+                name="email"
+                required
+                placeholder="이메일을 입력하세요"
+              />
+              <span className="error">{emailMsg}</span>
+            </div>
+            <div className="form__item">
+              <legend>패스워드</legend>
+              <input
+                type="password"
+                onChange={handleAuthInfo}
+                name="password"
+                required
+                placeholder="패스워드를 입력하세요"
+              />
+              <span className="error">{passMsg}</span>
+            </div>
+            <div className="form__item">
+              <legend>패스워드 확인</legend>
+              <input
+                type="password"
+                onChange={handleAuthInfo}
+                name="passwordConfirm"
+                required
+                placeholder="다시 한번 입력하세요"
+              />
+              <span className="error">{passwdMsg}</span>
+            </div>
+            <div className="form__item">
+              <input type="submit" value="회원가입" onClick={onSignUp} />
+            </div>
+            <hr />
+            <p className="info">
+              <Link to="/auth/login" className="btn">
+                로그인 화면으로 돌아가기
+              </Link>
+            </p>
+          </div>
+        </>
       ) : (
         <Redirect to="/service" />
       )}
