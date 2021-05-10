5529be304e6485bcf1b90a30cd86ed004a3f427f
G1 <g1.jeon@kakao.com>
2021-05-11 08:57:35 +0900

    fix: AuthLayout Style

--- frontend/src/shared/layouts/AuthLayout.js
+++ frontend/src/shared/layouts/AuthLayout.js
@@ -5,6 +5,7 @@ import './AuthLayout.scss';
 export default function AuthLayout({ children }) {
     return (
         <section className="form-wrapper">
+            <h1>사이버수사관<br />모니터링업무보조시스템</h1>
             <div className="cover"></div>
             <form className="form form__auth">
                 { children }
