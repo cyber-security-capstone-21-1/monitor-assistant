f48f9ff13e12463b8c6333403a2d58674cb749ab
G1 <g1.jeon@kakao.com>
2021-05-10 14:44:29 +0900

    Update IntList.js

--- frontend/src/pages/IntList.js
+++ frontend/src/pages/IntList.js
@@ -20,14 +20,14 @@ function IntList () {
         <>
             <PageHeader title="첩보 목록" desc="첩보 목록" />
             <section style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px" }}>
-                {intList.map(({ title, site, author, created_at, archived_UID, id, content }, index) => {
+                {intList.map(({ title, site, author, created_at, uid, id, content }, index) => {
                     return (
                         <IntelligenceCard
                             title={title}
                             site={site}
                             author={author}
                             created_at={created_at}
-                            uid={archived_UID}
+                            uid={uid}
                             content={content}
                         />
                     )
