edfe506d38f1797e6abf74634369ce96fe558039
G1 <g1.jeon@kakao.com>
2021-05-10 17:32:42 +0900

    Update UserAuthenticationController.java

--- src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/controller/UserAuthenticationController.java
+++ src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/controller/UserAuthenticationController.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -38,6 +41,7 @@ public class UserAuthenticationController {
                             .build()
             ).getId();
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.ok(result);
