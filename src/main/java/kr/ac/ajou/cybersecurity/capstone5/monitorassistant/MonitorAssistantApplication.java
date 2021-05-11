64b3d92ded72023d902911a3522efa436053868e
junghyun2ee <ju2173@ajou.ac.kr>
2021-05-11 19:41:58 +0900

    Update MonitorAssistantApplication.java

--- src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/MonitorAssistantApplication.java
+++ src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/MonitorAssistantApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class MonitorAssistantApplication {
 
-	static {
-		System.setProperty("spring.config.location", "classpath:/application.yml,classpath:/aws.yml");
-	}
+//	static {
+//		System.setProperty("spring.config.location", "classpath:/application.yml,classpath:/aws.yml");
+//	}
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(MonitorAssistantApplication.class)
