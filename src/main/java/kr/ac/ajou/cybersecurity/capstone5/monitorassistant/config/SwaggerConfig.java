93841e1ff769c3afeebe907b8021baf8e62ec0ef
G1 <g1.jeon@kakao.com>
2021-05-11 16:18:53 +0900

    Update SwaggerConfig.java

--- src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/config/SwaggerConfig.java
+++ src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/config/SwaggerConfig.java
@@ -3,19 +3,27 @@ package kr.ac.ajou.cybersecurity.capstone5.monitorassistant.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI monitorAssistantAPI() {
+        Server server = new Server();
+        server.setUrl("https://monitor-assistant.com/api");
         return new OpenAPI()
-                .info(new Info().title("사이버수사관모니터링업무보조시스템 API")
+                .servers(List.of(server))
+                .info(new Info()
+                        .title("사이버수사관모니터링업무보조시스템 API")
                         .description("API 명세")
                         .version("v1")
-                        .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+                        .license(new License().name("Apache 2.0").url("http://www.apache.org/licenses/LICENSE-2.0.html"))
+                );
     }
 
 }
