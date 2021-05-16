47a8b0ab2ebe61239722a86b3a084d6e973cfe74
G1 <g1.jeon@kakao.com>
2021-05-16 13:57:28 +0900

    fix: Intelligence Response when saving

--- src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/repositories/IntelligenceRepository.java
+++ src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/repositories/IntelligenceRepository.java
@@ -2,8 +2,6 @@ package kr.ac.ajou.cybersecurity.capstone5.monitorassistant.repositories;
 
 import kr.ac.ajou.cybersecurity.capstone5.monitorassistant.entities.IntelligenceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
