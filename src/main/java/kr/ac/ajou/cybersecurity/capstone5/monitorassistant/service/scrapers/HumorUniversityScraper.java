7564ed25d458c886df2a4114714f924a5ca5141e
junghyun2ee <ju2173@ajou.ac.kr>
2021-05-29 00:46:09 +0900

    마무리

--- src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/service/scrapers/HumorUniversityScraper.java
+++ src/main/java/kr/ac/ajou/cybersecurity/capstone5/monitorassistant/service/scrapers/HumorUniversityScraper.java
@@ -23,11 +23,11 @@ public class HumorUniversityScraper implements Scraper {
     @Override
     public List<PostEntity> getPosts(String keyword) throws IOException, ParseException {
         List<PostEntity> list = new ArrayList<>();
-        Document doc[] = new Document[3];
+        Document doc[] = new Document[2];
         String encodeKeyword = URLEncoder.encode(keyword, "EUC-KR");
 
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 2; i++) {
             Connection.Response response =
                     Jsoup.connect(HUMORUNIVERSITY_CRAWL_DATA_URL + encodeKeyword + "&search_type=&x=0&y=0&page=" + i + 1)
                             .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36")
